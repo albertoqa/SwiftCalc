2aefec59208265b7be1beeb6fd8b3e903e05be9a
Alberto Quesada Aranda <alberto@Albertos-MacBook-Pro.local>
2015-04-25 19:48:23 +0200

    display label and digit buttons added

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var isSetToZero: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        // var = variable, let = constante
+        let digit = sender.currentTitle!
+        if(!isSetToZero) {
+            display.text = display.text! + digit
+        }
+        else {
+            display.text = digit
+            isSetToZero = false
+        }
+        
+        //println("digit = \(digit)")
+        
+    }
 
 }
 
