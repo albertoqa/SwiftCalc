444c081cd32f2eec4f01a3e0fb40e6978e46ea94
Alberto Quesada Aranda <alberto@Albertos-MacBook-Pro.local>
2015-04-25 18:27:55 +0200

    Xcode project created - Swift/Universal

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow? // new to swift
+    var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
